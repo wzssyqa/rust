58d9280d394ab0269d5d39b7e99b8ab628db9e7d
Matthew Jasper <mjjasper1@gmail.com>
2019-02-02 22:49:44 +0000

    Update tests for restrictive two-phase borrows

--- src/test/ui/borrowck/two-phase-reservation-sharing-interference-2.rs
+++ src/test/ui/borrowck/two-phase-reservation-sharing-interference-2.rs
@@ -2,23 +2,12 @@
 
 // This is similar to two-phase-reservation-sharing-interference.rs
 // in that it shows a reservation that overlaps with a shared borrow.
-//
-// Currently, this test fails with lexical lifetimes, but succeeds
-// with non-lexical lifetimes. (The reason is because the activation
-// of the mutable borrow ends up overlapping with a lexically-scoped
-// shared borrow; but a non-lexical shared borrow can end before the
-// activation occurs.)
-//
-// So this test is just making a note of the current behavior.
 
-#![feature(rustc_attrs)]
-
-#[rustc_error]
-fn main() { //~ ERROR compilation successful
+fn main() {
     let mut v = vec![0, 1, 2];
     let shared = &v;
 
-    v.push(shared.len());
+    v.push(shared.len()); //~ ERROR cannot borrow `v` as mutable
 
     assert_eq!(v, [0, 1, 2, 3]);
 }
